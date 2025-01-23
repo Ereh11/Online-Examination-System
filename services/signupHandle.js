6941d1522255ce917ca2fe301c3dd87abca284eb
‪Hany Abdou Saad‬‏ <Hanysaadstd@gmail.com>
2025-01-23 07:08:15 +0200

    feat: implement user creation and database submission functionality

--- services/signupHandle.js
+++ services/signupHandle.js
@@ -1,4 +1,4 @@
-import { User } from '../models/user.js';
+import { User } from "../models/user.js";
 
 let newUser;
 const urlUsers = "http://localhost:3000/users";
@@ -11,44 +11,64 @@ const urlUsers = "http://localhost:3000/users";
  * @returns {User Object}
  */
 function creatUser(username, email, password) {
-    const user = new User(username, email, password);
-    return user;
+  const user = new User(username, email, password);
+  return user;
 }
-
 /**
  * Check if user already exists in the database or not
  * @param {string} email
  * @returns {Promise<boolean>} true if user exists, false if user does not exist
  */
 async function checkUserExistence(email) {
-    try 
-    {
-        const response = await fetch(urlUsers);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        const userExists = data.some(user => user.email === email);
-        return userExists; 
-    } catch (error) {
-        console.error('Error fetching JSON data:', error);
-        return false;
+  try {
+    const response = await fetch(urlUsers);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+    const data = await response.json();
+    const userExists = data.some((user) => user.email === email);
+    return userExists;
+  } catch (error) {
+    console.error("Error fetching JSON data:", error);
+    return false;
+  }
 }
-
 /**
  * Receive data from the signup form and create a new user
- * @param {string} username 
- * @param {string} email 
- * @param {string} password 
+ * @param {string} username
+ * @param {string} email
+ * @param {string} password
  * @returns {void}
  */
 export async function recievedData(username, email, password) {
-    newUser = creatUser(username, email, password);
-    const userExists = await checkUserExistence(newUser.email);
-    if (userExists) {
-        window.location.href = "../pages/login.html";
-    } else {
-        alert("User not exists");
+  newUser = creatUser(username, email, password);
+  const userExists = await checkUserExistence(newUser.email);
+  if (userExists) {
+    window.location.href = "../pages/login.html";
+  } else {
+    try {
+      sendUserToDatabase(newUser);
+      window.location.href = "../pages/login.html";
+    } catch (error) {
+      console.error("Error sending user to database:", error);
     }
+  }
+}
+/**
+ * Send the new user object to the database
+ * @param {User Object} user
+ * @returns {void}
+ */
+async function sendUserToDatabase(user) {
+  const response = await fetch(urlUsers, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
 }
