ed513257fd1596493a7926228293b6291b6a4be9
‪Hany Abdou Saad‬‏ <Hanysaadstd@gmail.com>
2025-01-27 08:10:27 +0200

    refactor: adjust image size in exam result and update score calculation to percentage

--- scripts/exam-result.js
+++ scripts/exam-result.js
@@ -1,6 +1,7 @@
 // Example of getting the grade dynamically (can be retrieved from URL or API)
 const queryParams = new URLSearchParams(window.location.search);
-const score = queryParams.get("examresult") || "F"; // Default to 'F' if not provided
+let score = queryParams.get("examresult") || 0; // Default to 'F' if not provided
+score = (parseInt(score) / 10) * 100; // Convert to percentage
 const grade = makeGrade(score);
 
 // Display the grade on the page
@@ -33,7 +34,6 @@ if (grade === "F") {
  * @returns {String} grade A, B, C, D, or F
  */
 function makeGrade(score) {
-    score = (parseInt(score) / 10) * 100;
     console.log(score);
     if (score >= 80) {
         return "A";
