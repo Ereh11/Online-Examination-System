91248b90199f93ccb3a0f8a6c048eacae4c8e44e
‪Hany Abdou Saad‬‏ <Hanysaadstd@gmail.com>
2025-02-14 17:41:50 +0200

    feat: update CSS styles for improved layout and responsiveness; enhance user profile score display with conditional formatting

--- scripts/user-profile.js
+++ scripts/user-profile.js
@@ -1,17 +1,24 @@
-import { getResults } from "../services/resultExamHandle.js"
+import { getResults } from "../services/resultExamHandle.js";
 
 const tbody = document.querySelector("tbody");
 const email = localStorage.getItem("email");
 const userInfoDiv = document.getElementsByClassName("user-info")[0];
 const emailDom = userInfoDiv.children[0];
 const resultUser = await getResults(email);
-
 emailDom.innerText = email;
-console.log(resultUser);
-resultUser.forEach(element => {
-  tbody.innerHTML += ` <tr>
-              <td>${element.examName}</td>
-              <td>${element.score}</td>
-            </tr>
-          `;
+
+resultUser.forEach((element) => {
+  if (element.score < 50) {
+    tbody.innerHTML += ` <tr>
+                <td>${element.examName}</td>
+                <td style ="background-color:rgb(201, 71, 71) ; color: white" >${element.score}</td>
+              </tr>
+            `;
+  } else {
+    tbody.innerHTML += ` <tr>
+                <td>${element.examName}</td>
+                <td style ="background-color:rgb(112, 197, 90) ; color: white" >${element.score}</td>
+              </tr>
+            `;
+  }
 });
